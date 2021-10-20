b2a26c76945ac89ec44baafbf730b203f9a1e420
Emmanuel Valverde Ramos <evrtrabajo@gmial.com>
2021-10-20 16:59:48 +0200

    make sure that the arrays have the same length

--- src/main/java/MergeTwoArrays.java
+++ src/main/java/MergeTwoArrays.java
@@ -3,6 +3,9 @@ public class MergeTwoArrays {
         if (null == firstArray || null == secondArray) {
             throw new IllegalArgumentException("The method expected 2 arrays and null was given");
         }
+        if (firstArray.length != secondArray.length) {
+            throw new IllegalArgumentException("The method expected 2 arrays with the same length");
+        }
         return new int[0];
     }
 }
--- src/test/java/MergeTwoArrayTest.java
+++ src/test/java/MergeTwoArrayTest.java
@@ -9,4 +9,11 @@ public class MergeTwoArrayTest {
             MergeTwoArrays.mergeArrays(null, null);
         });
     }
+
+    @Test
+    public void should_check_that_two_arrays_have_the_same_length(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            MergeTwoArrays.mergeArrays(new int[3], new int[4]);
+        });
+    }
 }
