9c0f4c168c7a3b212dab5a0f03343584401256cd
Emmanuel Valverde Ramos <evrtrabajo@gmial.com>
2021-10-21 08:34:54 +0200

    added restriction to have arrays with the correct size to be chunked

--- src/main/java/MergeTwoArrays.java
+++ src/main/java/MergeTwoArrays.java
@@ -6,6 +6,25 @@ public class MergeTwoArrays {
         if (firstArray.length != secondArray.length) {
             throw new IllegalArgumentException("The method expected 2 arrays with the same length");
         }
-        return new int[0];
+        int MAX_CHUNK = 3;
+        if (firstArray.length % MAX_CHUNK != 0) {
+            throw new IllegalArgumentException("The arrays have to be dividable by " + MAX_CHUNK);
+        }
+
+        int totalOfElementsByIteration = (firstArray.length*2) / MAX_CHUNK; // 6
+        int [] result =  new int[firstArray.length*2];
+
+        for(int i = 0; i < result.length; i += totalOfElementsByIteration) {
+            for (int j = 0; j < MAX_CHUNK;j++) {
+                result[i+j] = firstArray[(i / 2)+j];
+            }
+
+            for (int j = 0; j < MAX_CHUNK;j++) {
+                result[i+(j + MAX_CHUNK)] = secondArray[(i / 2)+j];
+            }
+
+        }
+
+        return result;
     }
 }
--- src/test/java/MergeTwoArrayTest.java
+++ src/test/java/MergeTwoArrayTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MergeTwoArrayTest {
     @Test
@@ -16,4 +16,28 @@ public class MergeTwoArrayTest {
             MergeTwoArrays.mergeArrays(new int[3], new int[4]);
         });
     }
+
+    @Test
+    public void should_return_an_array_with_the_double_of_length(){
+        int [] firstArray = {1,2,3,4,5,6,7,8,9};
+        int [] secondArray = {10,11,12,13,14,15,16,17,18};
+        int [] expectedResult = {1,2,3,10,11,12,4,5,6,13,14,15,7,8,9,16,17,18};
+        assertEquals(expectedResult.length, MergeTwoArrays.mergeArrays(firstArray, secondArray).length);
+    }
+
+    @Test
+    public void should_merge_two_arrays_on_chunks_of_three(){
+        int [] firstArray = {1,2,3,4,5,6,7,8,9};
+        int [] secondArray = {10,11,12,13,14,15,16,17,18};
+        int [] expectedResult = {1,2,3,10,11,12,4,5,6,13,14,15,7,8,9,16,17,18};
+        assertArrayEquals(expectedResult, MergeTwoArrays.mergeArrays(firstArray, secondArray));
+    }
+
+    @Test
+    public void should_not_merge_two_arrays_with_non_divisional_by_chunks(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            MergeTwoArrays.mergeArrays(new int[4], new int[4]);
+        });
+    }
+
 }
